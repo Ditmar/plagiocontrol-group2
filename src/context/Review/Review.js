255095dcd9411a1924af0ead26657fb9cb41e583
Ditmar <ditmarcastro@gmail.com>
2022-06-28 21:55:38 -0400

    Revert "feature(common):new feature PC2-4"
    This reverts commit 80d07a1d55eb10bef8f2111a775ad0dec2f35be6.

--- src/context/Review/Review.js
+++ src/context/Review/Review.js
@@ -1,28 +1,9 @@
 import React from "react";
-import Title from "../../hooks/Title";
-import Usuario from "../../hooks/Usuario";
-import "../../Review.css";
-import Busqueda from "../../hooks/Busqueda";
-import Bell from "../../hooks/Bell";
-
 
 export const Review = () => {
-
-    const user= {
-        nombre:'Lisa Marie Simpsom​',
-        urlImagen: 'https://i.pinimg.com/736x/25/36/35/253635aa6fe30a74ce5833fa99659a54.jpg',
-    };
-
-
     return (
-        <div className="padre">
-            <div className="hijo"><Title Pagina="/ Header" /></div>
-            <div className="hijo"><Busqueda /></div>
-            <div className="hijo"><Bell /> </div>
-            <div className="hijo"> <Usuario user={user}/></div>
+        <div>
+            Review Page
         </div>
-        
-     
-        
     )
 }
