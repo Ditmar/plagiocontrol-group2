4f597c2bc7322959110b0f1e10240737f13714dd
leonel <fer85leo85@gmail.com>
2022-07-04 16:22:30 -0400

    feature(common):deleted changes in last document

--- src/context/Works/Works.js
+++ src/context/Works/Works.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Sidebar_menu from "../../ui/components/menuStorybook/Sidebar_menu";
 
 export const Works = () => {
     return (
         <div>
-           <Sidebar_menu/> 
+            Works page
         </div>
     )
 }
