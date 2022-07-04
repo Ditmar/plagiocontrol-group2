a682d21f891834fcfc11463fafa58c3479cdcfd2
unknown <anahiflores7878@gmail.com>
2022-07-03 21:12:28 -0400

    feature(common): Document Card

---
+++ src/ui/components/DocumentCard/SuperDocumentCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DocumentCard from './DocumentCard';
+
+function SuperDocumentCard(){
+    return (
+        <div className='container-superDocumentCard'>
+            <DocumentCard name='Ing. Raul Gomez' description='Analisis de la investigacion sobre radio Bolivia..' numberPage='321' imagePage='http://3.bp.blogspot.com/_getYKOqtOUs/TKP_YZOMdFI/AAAAAAAAAbk/7uFUrSF1skY/s1600/tesis.JPG'/>
+        </div>
+    )
+}
+
+export default SuperDocumentCard;
