8e2b01c626e1aa9a3c0fbeeeabd9d92b285abb02
Cristofer Goméz <cristofergm123@gmail.com>
2022-07-06 03:46:16 -0400

    feat(common): add and update login [pc-16]

--- src/ui/components/button/button.stories.js
+++ src/ui/components/button/button.stories.js
@@ -9,17 +9,6 @@ export default {
 const Template = args => <Button  {...args}/>
 export const DefaultButton = Template.bind({});
 DefaultButton.args = {
-    label: 'Press Me',
-    backgroundColor: '#3751FF',
-    isShadow: true,
+    label: 'Log In',
     size: 'md'
 }
-
-export  const PrimaryButton = Template.bind({});
-PrimaryButton.args = {
-    label: 'Primary',
-    backgroundColor: '#e338ff',
-    isShadow: false,
-    size: 'lg'
-}
-
