42646a55f98bdbf8757c1f18714e6f71e08fb276
mauricio condori <condorimauricio6@gmail.com>
2022-07-11 16:18:05 -0400

    fix names files

---
+++ src/ui/components/listar/Listar.stories.js
@@ -0,0 +1,16 @@
+import React from "react";
+//import { storiesOf } from "@storybook/react";
+//import { action } from "@storybook/addon-actions";
+
+import { Listar } from "./Listar";
+export default {
+    title: "ui/components/listar",
+    component: Listar,
+}
+const Template = (args) => <Listar {...args}/>
+export  const logue= Template.bind({}); 
+logue.args = {
+    isLoading: false,
+    label: "test", 
+    errors: false
+}
