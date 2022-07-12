0e1336e47f375ba49ac20f5afac4708bd1d26354
NoemiCoro <noemicorovasquez@gmail.com>
2022-07-12 19:37:42 -0400

    feature(common): edited according to figma [PC2-7]

--- src/context/Home/Home.js
+++ src/context/Home/Home.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { LastDocument } from "../../ui/components/lastDocuments.jsx/LastDocument";
-import { CardHome } from "../../ui/components/cardhome/CardHome"
-import "./Homes.css"
+import React from 'react';
+import { LastDocument } from '../../ui/components/lastDocuments/LastDocument';
+import { CardHome } from '../../ui/components/cardhome/CardHome';
+import './Homes.css'
 
 export const Home = () => {
     return <div className="style">
