205cdcf8ab4f0400b0d7c30d55bc7d150aaa3d31
NoemiCoro <noemicorovasquez@gmail.com>
2022-07-12 23:11:41 -0400

    feature (common): editing card home [PC2-7]

--- src/context/Home/Home.js
+++ src/context/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LastDocument } from '../../ui/components/lastDocuments/LastDocument';
+import { LastDocument } from '../../ui/components/lastDocuments.jsx/LastDocument';
 import { CardHome } from '../../ui/components/cardhome/CardHome';
 import './Homes.css'
 
