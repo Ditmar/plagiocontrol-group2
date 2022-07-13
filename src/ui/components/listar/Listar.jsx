829d2b9710ede013eade1adaed8576b7139931d6
Mauricio <condorimauricio6@gmail.com>
2022-07-13 11:47:46 -0500

    added Data in other file json

--- src/ui/components/listar/Listar.jsx
+++ src/ui/components/listar/Listar.jsx
@@ -20,54 +20,29 @@ import {
     TextField
  } from '@material-ui/core';
 
- import TablePagination from "./tablepagination"
- 
+ import TablePagination from "./tablepagination";
+ import data from "./Datos.json";
  
 
 
 
 
 export const Listar = ({}) => {
-  const [data, setData] = useState([
-    {id: 1, detalle1:'Contact Email not Linked',detalle2:'Updated 1 day ago',
-    autor:'Tom Cruise',fecha:'on 24/05/2019',
-    hora:'6:30 PM',descarga:'WNL'},
-    {id: 2, detalle1:'Adding Images to Featured Posts',detalle2:'Updated 1 day ago',
-    autor:'Matt Damon',fecha:'on 24/05/2019',
-    hora:'9:00 AM',descarga:'LOW'},
-    {id: 3, detalle1:'When will l be charged this month?',detalle2:'prueba',
-    autor:'Robert Downey',fecha:'on 24/05/2019',
-    hora:'5:00 PM',descarga:'HIGH'},
-    {id: 4, detalle1:'Payment not going through',detalle2:'Updated 2 days ago',
-    autor:'Christian Bale',fecha:'on 25/05/2019',
-    hora:'5:00 PM',descarga:'NORMAL'},
-    {id: 5, detalle1:'Unable to add replies',detalle2:'Updated 2 days ago',
-    autor:'Henry Cavil',fecha:'on 24/05/2019',
-hora:'4:00 PM',descarga:'HIGH'},
-{id: 6, detalle1:'Downtime since last week',detalle2:'Updated 3 days ago',
-autor:'Chris Evans',fecha:'on 23/05/2019',
-hora:'2:00 PM',descarga:'NORMAL'},
-{id: 7, detalle1:'Referral Bonus',detalle2:'Updated 4 days ago',
-autor:'Sam Smith',fecha:'on 22/05/2019',
-hora:'11:30 AM',descarga:'LOW'},
-{id: 8, detalle1:'How do I change my password',detalle2:'Updated 6 days ago',
-autor:'Steve Rogers',fecha:'on 21/05/2019',
-hora:'1:00 PM',descarga:'NORMAL'},
-])
+const [info,setInfo]=useState(data); 
 const [order,setOrder]= useState("ASC")
 const sorting= (col)=> {
 if (order==="ASC") {
-const sorted = [...data].sort((a,b)=>
+const sorted = [...info].sort((a,b)=>
 a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
 );
-setData(sorted);
+setInfo(sorted);
 setOrder("DSC");
 }
 if (order==="DSC") {
-const sorted=[...data].sort((a,b)=>
+const sorted=[...info].sort((a,b)=>
 a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
 );
-setData(sorted);
+setInfo(sorted);
 setOrder("ASC");
 } 
 };
@@ -95,24 +70,24 @@ return (
          </TableRow>
       </TableHead>
       <TableBody>
-      {data.filter((row)=>row.autor.toLowerCase().includes(query)).map((row) => (
-            <TableRow key={row.id}>      
+      {info.filter((info)=>info.autor.toLowerCase().includes(query)).map((info) => (
+            <TableRow key={info.id}>      
                 <TableCell>
                 <Grid container>
                   <Grid item lg={1}>
                       <Avatar  src='https://img.freepik.com/foto-gratis/cierrese-encima-retrato-cara-joven-hombre-barbudo_171337-2887.jpg?w=2000'/>
                   </Grid>
                   <Grid item lg={10}>
-                      <Typography ><span className="detalle">{row.detalle1}</span></Typography>
-                      <Typography><span className="detalle2">{row.detalle2}</span></Typography>
+                      <Typography ><span className="detalle">{info.detalle1}</span></Typography>
+                      <Typography><span className="detalle2">{info.detalle2}</span></Typography>
                       
                   </Grid>
               </Grid>
                </TableCell>
                 <TableCell>
                 <Grid item lg={10}>
-                      <Typography><span className="name">{row.autor}</span></Typography>
-                      <Typography ><span className="fecha">{row.fecha}</span></Typography>
+                      <Typography><span className="name">{info.autor}</span></Typography>
+                      <Typography ><span className="fecha">{info.fecha}</span></Typography>
                       
                     </Grid>
                 </TableCell>
@@ -120,7 +95,7 @@ return (
                 <TableCell>
                 <Grid item lg={10}>
                     <FiEdit className="editar"/>
-                      <Typography><span className="tiempo">{row.hora}</span></Typography>
+                      <Typography><span className="tiempo">{info.hora}</span></Typography>
                       
                     </Grid>
                 </TableCell>
@@ -130,7 +105,7 @@ return (
                 <Grid item lg={15}>
                 <Button >
                   
-                  <span className="b4">  {row.descarga}</span></Button>
+                  <span className="b4">  {info.descarga}</span></Button>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <BsThreeDotsVertical/>
                        
                        
