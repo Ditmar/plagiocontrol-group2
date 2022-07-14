49ba19e8c1b842966acbfc89721576fd929b4069
Cristofer Goméz <cristofergm123@gmail.com>
2022-07-14 19:09:49 -0400

    feature(common): connect the login to the API [PC-16]

--- src/ui/components/button/button.jsx
+++ src/ui/components/button/button.jsx
@@ -1,5 +1,5 @@
-import { PropTypes } from "prop-types";
-import { React } from "react"; 
+import { PropTypes } from 'prop-types';
+import { React } from 'react'; 
 export const Button = ({label, handleClick, size}) => {
     let scale = 18;
     if (size === 'lg') scale = 30.875;
--- src/ui/components/loginT/login.jsx
+++ src/ui/components/loginT/login.jsx
@@ -1,43 +1,37 @@
-import React from "react";
+import React from 'react';
 import './login.css';
-import { useState } from "react";
-import useForm from "../../../hooks/useForm";
+import { useState } from 'react';
+import useForm from '../../../hooks/useForm';
+import axios from 'axios';
 
 
 const LoginUI = () => {
-    const [form, handlerChangeForm] = useForm({user:'', password:''});
-    const { user, password } = form;
+    const [form, handlerChangeForm] = useForm({email:'', password:''});
+    const { email, password } = form;
+    let seePass = 'password';
+    
+    //const urldb = 'http://localhost:8000/server/autenthication/login';
+    /* 
+    The API crashed a lot, for this reason an API was used with the same parameters as the main API
+    */
+    const url = 'http://localhost:3001/usuario';
+
+    const startSesion = async(email, password) => {
+        await axios.get(url, {params: {email: email, password: password}})
+        .then(response=>{
+            console.log(response.data);
+        });
+    }
+
     const handlerSubmit = (event) => {
         event.preventDefault();
-        console.log('user:',user,'pass:',password);
+        //console.log('email:',email,'pass:',password);
+        startSesion(email,password);
     }
-    //const urldb = 'http://localhost:8000/server/autenthication/login';
-    /*const [us, setUs] = useState('');
-    const [pass, setPass] = useState('');
-    const handlerChange = (name, value) => {
-        if (name === 'user')    setUs(value);
-        else setPass(value);
-    }
-    const handlerSubmit = () => {
-        let valor = { us, pass};
-        if (valor) {
-            //console.log('valor:',valor);
-            Validar(valor);
-        }
-    }
-    (e) => handlerChange(e.target.name, e.target.value)
-    const Validar = (parametro) => {
-        
-    }*/
 
-    
-    //console.log('Usuario:', us);
-    //console.log('Usuario:', pass);
-    const mostrarPass = () => {
-        let tipo = document.getElementById("password");
-        console.log(tipo);
-        if (tipo == "password") tipo = "text";
-        else tipo = "password";
+    const seePassword = () => {
+        if (seePass === "password") seePass = "text";
+        else seePass = "password";
     }
     return (
         <div className="main">
@@ -45,26 +39,26 @@ const LoginUI = () => {
                 <p className="d"></p>
             </div>
             <h3 className="logoL">Plagio Control</h3>
-            <h1 className="titulo">Login Plagio Control</h1>
-            <p className="comentario">Enter you email and password below</p>
+            <h2 className="tit">Login Plagio Control</h2>
+            <p className="comment">Enter you email and password below</p>
             <form onSubmit={handlerSubmit}>
-                <div className="conjunto">
+                <div className="con">
                     <label htmlFor="">EMAIL</label>
-                    <input className="dato" type="text" placeholder="Email address" name="user" onChange={handlerChangeForm} />
+                    <input className="in" type="text" placeholder="Email address" name="email" onChange={handlerChangeForm} />
                 </div>
-                <div className="conjunto">
+                <div className="con">
                     <label htmlFor="">PASSWORD</label>
-                    <a href="" className="olvidaste">Forgot password?</a>
-                    <input className="dato" type="password" placeholder="Password" name="password" id="password" onChange={handlerChangeForm} />
-                    <button className="ojoB">
-                        <img className="ojo" src="https://cdn-icons-png.flaticon.com/512/7042/7042918.png" />
+                    <a href="" className="forgot">Forgot password?</a>
+                    <input className="in" type={seePass} placeholder="Password" name="password" onChange={handlerChangeForm} />
+                    <button className="eyeB" onClick={seePassword}>
+                        <img className="eye" src="https://cdn-icons-png.flaticon.com/512/7042/7042918.png" />
                     </button>
                 </div>
-                <button className="boton">Log In</button>
+                <button className="button">Log In</button>
                 <div>
-                    <a href="">No tienes una cuanta</a>
+                    <a>No tienes una cuanta?</a>
                     &nbsp;&nbsp;
-                    <a href="" className="singup">Sing up</a>
+                    <a href="/Register" className="singup">Sing up</a>
                 </div>
             </form>
         </div>
